c4aa2e4b0c0e60f3617420a96a1271f0b9d6374b
SAlberte <switala.alb@gmail.com>
2023-01-17 23:51:30 +0100

    Prepare for deployment

--- post_install.sh
+++ post_install.sh
@@ -4,3 +4,4 @@ tar -xf /tmp/img.tar.gz -C /var/www/html && rm /tmp/img.tar.gz
 tar -xf /tmp/mails.tar.gz -C /var/www/html && rm /tmp/mails.tar.gz
 tar -xf /tmp/localization.tar.gz -C /var/www/html && rm /tmp/localization.tar.gz
 tar -xf /tmp/themes.tar.gz -C /var/www/html && rm /tmp/themes.tar.gz
+chown -R www-data:www-data /var/www/html
