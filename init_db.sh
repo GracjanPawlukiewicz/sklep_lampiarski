44be30b13f73182df9c0764b79cb9e4f09f7dc6a
Mateusz Duchalski <mduchalski1@gmail.com>
2023-01-17 23:51:30 +0100

    Prepare for deployment

--- init_db.sh
+++ init_db.sh
@@ -5,14 +5,13 @@ if [ "$#" -eq 1 ]; then
     MODE=$1
 fi
 
-DB_CONT="mysql"
+DB_CONT="actina15.maas"
 DB_GLOB_USER="root"
 DB_GLOB_PWD="student"
-DB_NAME_USER="Lumos"
+DB_NAME_USER="BE_171547"
 DB_PWD="lampexpol"
 BACKUP_FILE="backup/1671232069-61a49c55.sql.bz2"
-DOMAIN="localhost:8080" # Unused
-DOMAIN_SSL="localhost:10001"
+DOMAIN="localhost:17154" # Review change for deployment
 
 echo "DROP DATABASE IF EXISTS ${DB_NAME_USER};
 CREATE DATABASE ${DB_NAME_USER};
@@ -26,4 +25,4 @@ fi
 
 bunzip2 < $BACKUP_FILE | docker exec -i $DB_CONT mysql -u $DB_NAME_USER -p$DB_PWD -D $DB_NAME_USER
 
-echo "UPDATE ps_shop_url SET domain='${DOMAIN}',domain_ssl='${DOMAIN_SSL}',physical_uri='/';" | docker exec -i $DB_CONT mysql -u $DB_NAME_USER -p$DB_PWD -D $DB_NAME_USER
+echo "UPDATE ps_shop_url SET domain='${DOMAIN}',domain_ssl='${DOMAIN}',physical_uri='/';" | docker exec -i $DB_CONT mysql -u $DB_NAME_USER -p$DB_PWD -D $DB_NAME_USER
